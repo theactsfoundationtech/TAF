1384a0d1c6dac089193a55900088483aff681f30
jharvarddevs <jharvarddevs@gmail.com>
2026-01-20 02:35:18 -0500

    chore: change output directory to 'dist' and ignore TS errors for broader hosting compatibility

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: { unoptimized: true },
   trailingSlash: true,
